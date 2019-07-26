037a513ea131e7df6668eb1654c6ffe2529c9676
19j01acs039 <50985270+19j01acs039@users.noreply.github.com>
2019-07-26 16:23:18 +0300

    allan

---
+++ src/allan/Allan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allan;
+
+import home.patient;
+
+/**
+ *
+ * @author User
+ */
+public class Allan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        patient pa=new patient();
+        pa.setVisible(true);
+    }
+    
+}
